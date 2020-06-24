87a597f9139534b0487aa3e2ef8a830f540652ca
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-06-24 10:34:01 -0400

    append moves to movelist

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ document.addEventListener("DOMContentLoaded", function(){
   renderBot(currentPosition)
 
   // ADD CODE HERE!
+  keyEvents = [ "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
+  document.addEventListener("keydown", function(event) {
+      let keyInput = event.key;
+      let moveList = document.getElementById("moves-container");
+
+      if (keyEvents.includes(keyInput)) {
+        // console.log(moveList.);
+        let listElement = document.createElement("li");
+            listElement.innerText = `${keyInput}`;
+        moveList.append(listElement)
+      }
+  })
 })
